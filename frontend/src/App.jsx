54ec17ca2593eb15e7eeb9c4d0c8de66c619c71d
Rohit <kumarrohit67476@gmail.com>
2025-09-15 08:04:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,7 @@ function FlightPricePredictor() {
     setLoading(true);
     try {
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+        "${process.env.REACT_APP_API_URL}/predict",
         formData
       );
       setPrediction(response.data.prediction);
